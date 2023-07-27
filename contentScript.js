a65e6cc533507d4ff0e39a26264415aa2d5dd93f
zeepc <zee.perez-canals@riskified.com>
2023-07-27 12:52:09 -0400

    change observer mutation to prevent caching old merchant data causing display issue for merchant exception

--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,6 @@
 // CHAT GPT 
 
-// Grab MID and Merchant Name 
-
+// Grab MID and Name, store it and then forget completely. Prevent caching from previous page load
 let observer = new MutationObserver((mutationsList, observer) => {
   for(let mutation of mutationsList) {
     if (mutation.type === 'childList') {
@@ -14,6 +13,13 @@ let observer = new MutationObserver((mutationsList, observer) => {
         // Store the merchant name and ID found on the page to chrome storage
         chrome.storage.local.set({merchantName: merchantName, merchantId: merchantId});
         
+        // Check if we have additional information for this merchant
+        const info = data[merchantId];
+        if (info) {
+          // If we do, show the popup
+          showPopup(info);
+        }
+
         // Once we've found the element, we don't need to observe anymore
         observer.disconnect();
         break;
@@ -24,62 +30,129 @@ let observer = new MutationObserver((mutationsList, observer) => {
 
 observer.observe(document.body, { childList: true, subtree: true });
 
-// grab data for info on the merchant
+function showPopup(info) {
+  // Create a popup div
+  var popupDiv = document.createElement('div');
+  popupDiv.textContent = info;
+  popupDiv.style.position = 'fixed';
+  popupDiv.style.top = '10px';
+  popupDiv.style.right = '10px';
+  popupDiv.style.backgroundColor = '#5A4CFF';
+  popupDiv.style.padding = '20px';
+  popupDiv.style.border = '2px solid white';
+  popupDiv.style.zIndex = 9999;
 
-chrome.storage.local.get(['merchantId'], function(result) {
-  const info = data[result.merchantId];
-  if (info) {
-    // If info exists for this merchant ID, show it
-    chrome.notifications.create({
-      type: 'basic',
-      iconUrl: 'icon.png',  // Path to your extension's icon or other suitable image
-      title: 'Information found',
-      message: info
-    });
-  } else {
-    // No additional information found
-  }
-});
+  // Create a close button
+  var closeButton = document.createElement('button');
+  closeButton.textContent = 'X';
+  closeButton.style.position = 'absolute';
+  closeButton.style.top = '0';
+  closeButton.style.right = '0';
+  closeButton.style.backgroundColor = 'transparent';
+  closeButton.style.border = 'none';
+  closeButton.style.cursor = 'pointer';
+  closeButton.style.fontSize = '20px';
 
-// notification pop up for data found instances 
+  // Add the click event listener to the close button
+  closeButton.addEventListener('click', function() {
+    document.body.removeChild(popupDiv);
+  });
 
-chrome.storage.local.get(['merchantId'], function(result) {
-  const info = data[result.merchantId];
-  if (info) {
-    // If info exists for this merchant ID, create a popup
-    var popupDiv = document.createElement('div');
-    popupDiv.textContent = info;
-    popupDiv.style.position = 'fixed';
-    popupDiv.style.top = '10px';
-    popupDiv.style.right = '10px';
-    popupDiv.style.backgroundColor = '#5A4CFF';
-    popupDiv.style.padding = '20px';
-    popupDiv.style.border = '2px';
-    popupDiv.style.zIndex = 9999;
+  // Add the close button to the popup div
+  popupDiv.appendChild(closeButton);
 
-    // Create a close button
-    var closeButton = document.createElement('button');
-    closeButton.textContent = 'X';
-    closeButton.style.position = 'absolute';
-    closeButton.style.top = '0';
-    closeButton.style.right = '0';
-    closeButton.style.backgroundColor = 'transparent';
-    closeButton.style.border = 'none';
-    closeButton.style.cursor = 'pointer';
-    closeButton.style.fontSize = '20px';
+  // Add the popup div to the page
+  document.body.appendChild(popupDiv);
+}
 
-    // Add the click event listener to the close button
-    closeButton.addEventListener('click', function() {
-      document.body.removeChild(popupDiv);
-    });
 
-    // Add the close button to the popup div
-    popupDiv.appendChild(closeButton);
 
-    // Add the popup div to the page
-    document.body.appendChild(popupDiv);
-  }
-});
+
+// ----- start --- caching issue presented. 
+// Grab MID and Merchant Name 
+
+// let observer = new MutationObserver((mutationsList, observer) => {
+//   for(let mutation of mutationsList) {
+//     if (mutation.type === 'childList') {
+//       let element = document.querySelector('body > div.cp-cockpit.ng-scope > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(3) > div > div:nth-child(2) > div.cp-value > span > a');
+//       if (element) {
+//         let merchantName = element.textContent;
+//         let href = element.getAttribute('href');
+//         let merchantId = href.split('/').pop();
+
+//         // Store the merchant name and ID found on the page to chrome storage
+//         chrome.storage.local.set({merchantName: merchantName, merchantId: merchantId});
+        
+//         // Once we've found the element, we don't need to observe anymore
+//         observer.disconnect();
+//         break;
+//       }
+//     }
+//   }
+// });
+
+// observer.observe(document.body, { childList: true, subtree: true });
+
+// // grab data for info on the merchant
+
+// chrome.storage.local.get(['merchantId'], function(result) {
+//   const info = data[result.merchantId];
+//   if (info) {
+//     // If info exists for this merchant ID, show it
+//     chrome.notifications.create({
+//       type: 'basic',
+//       iconUrl: 'icon.png',  // Path to your extension's icon or other suitable image
+//       title: 'Information found',
+//       message: info
+//     });
+//   } else {
+//     // No additional information found
+//   }
+// });
+
+// // notification pop up for data found instances 
+
+// chrome.storage.local.get(['merchantId'], function(result) {
+//   const info = data[result.merchantId];
+//   if (info) {
+//     // If info exists for this merchant ID, create a popup
+//     var popupDiv = document.createElement('div');
+//     popupDiv.textContent = info;
+//     popupDiv.style.position = 'fixed';
+//     popupDiv.style.top = '10px';
+//     popupDiv.style.right = '10px';
+//     popupDiv.style.backgroundColor = '#5A4CFF';
+//     popupDiv.style.padding = '20px';
+//     popupDiv.style.border = '2px';
+//     popupDiv.style.zIndex = 9999;
+
+//     // Create a close button
+//     var closeButton = document.createElement('button');
+//     closeButton.textContent = 'X';
+//     closeButton.style.position = 'absolute';
+//     closeButton.style.top = '0';
+//     closeButton.style.right = '0';
+//     closeButton.style.backgroundColor = 'transparent';
+//     closeButton.style.border = 'none';
+//     closeButton.style.cursor = 'pointer';
+//     closeButton.style.fontSize = '20px';
+
+//     // Add the click event listener to the close button
+//     closeButton.addEventListener('click', function() {
+//       document.body.removeChild(popupDiv);
+//     });
+
+//     // Add the close button to the popup div
+//     popupDiv.appendChild(closeButton);
+
+//     // Add the popup div to the page
+//     document.body.appendChild(popupDiv);
+//   }
+// });
+
+
+
+// ------end
 
 
 // try 3 WINNER WINNER CHICKEN DINNER
--- data.js
+++ data.js
@@ -1,6 +1,37 @@
+// sample data for testing usability on mvp
+// data being added to a csv to be converted to json via code for v2
+
 const data = {
   '12345': 'Some information about this merchant',
   '67890': 'Some information about another merchant',
-  '18567': 'Some exceptions for Eufy are as follows: Lorem ipsum'
+  '18567': 'Some exceptions for Eufy are as follows: Lorem ipsum',
+  '18451': 'Accept the following ACH reason codes R07 Authorization Revoked, R10 Customer Advises Not Authorized,R29  Corporate Customer Advises Not Authorized,R01 or WU_R01  Insufficient Funds',
+  '7558':  'Accept the following ACH reason codes R07 Authorization Revoked, R10 Customer Advises Not Authorized,R29 Corporate Customer Advises Not Authorized,R01 or WU_R01  Insufficient Funds',
+  '4932': 'Ignore Shipping and Chb notice, ACCEPT zara_banamex',
+  '18430': 'ignore shipping | Wolt Serbia (18430) ok to upload chb notice as xlsx file',
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
+  // '': ''
   // ... more data ...
 };
