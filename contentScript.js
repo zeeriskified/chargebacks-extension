41dd4fb6c002be2d4f3c748e8a6adb1fa2f617c4
zeepc <zee.perez-canals@riskified.com>
2023-07-28 17:19:30 -0400

    add css classes, create div for each data point

--- contentScript.js
+++ contentScript.js
@@ -2,6 +2,7 @@
 // parsing CSV, Observer, Display data 
 
 // Fetch and parse CSV
+
 let data;
 fetch(chrome.runtime.getURL('exceptions.csv'))
     .then(response => response.text())
@@ -22,7 +23,34 @@ let observer = new MutationObserver((mutationsList, observer) => {
                 if (info) {
                     // If info exists for this merchant ID, create a popup
                     var popupDiv = document.createElement('div');
-                    popupDiv.textContent = JSON.stringify(info);  // Convert object to string
+                    popupDiv.className = 'popupClass'; // add a class name to div for styling
+
+                    // Create a div to hold the data
+                    var infoDiv = document.createElement('div');
+                    infoDiv.className = 'infoClass';
+
+                    // Assume `info` is an object like { merchant_id: '123', name: 'Merchant name', field1: 'data1', field2: 'data2', ... }
+                    for (let key in info) {
+                        if (info.hasOwnProperty(key)) {
+                            let keyValueDiv = document.createElement('div');
+                            keyValueDiv.className = 'keyValueClass';
+
+                            let keyDiv = document.createElement('div');
+                            keyDiv.textContent = key;
+                            keyDiv.className = key; // here we assign the class name to be the key itself
+                            keyValueDiv.appendChild(keyDiv);
+
+                            let valueDiv = document.createElement('div');
+                            valueDiv.textContent = info[key];
+                            valueDiv.className = 'valueClass';
+                            keyValueDiv.appendChild(valueDiv);
+
+                            infoDiv.appendChild(keyValueDiv);
+                        }
+                    }
+
+                    popupDiv.appendChild(infoDiv);
+                    // styler to be moved overto style sheet
                     popupDiv.style.position = 'fixed';
                     popupDiv.style.top = '10px';
                     popupDiv.style.right = '10px';
@@ -32,7 +60,6 @@ let observer = new MutationObserver((mutationsList, observer) => {
                     popupDiv.style.zIndex = 9999;
                     popupDiv.style.position = 'fixed';
                     popupDiv.style.boxShadow = 'rgb(0 0 0 / 29%) 6px 10px 14px 2px';
-                      // '3px 5px 6px 1px rgba(6, 5, 23, 0.26)';
                     popupDiv.style.borderRadius = '5px';
                     popupDiv.style.height = '250px';
                     popupDiv.style.width = '350px';
@@ -76,129 +103,100 @@ observer.observe(document.body, { childList: true, subtree: true });
 
 
 
+//---- Works but class names for rows is not present here
 
 
+//let data;
+// fetch(chrome.runtime.getURL('exceptions.csv'))
+//     .then(response => response.text())
+//     .then(text => {
+//         data = Papa.parse(text, { header: true }).data;
+//     });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// CHAT GPT 
-
-// // Grab MID and Name, store it and then forget completely. Prevent caching from previous page load
 // let observer = new MutationObserver((mutationsList, observer) => {
-//   for(let mutation of mutationsList) {
-//     if (mutation.type === 'childList') {
-//       let element = document.querySelector('body > div.cp-cockpit.ng-scope > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(3) > div > div:nth-child(2) > div.cp-value > span > a');
-//       if (element) {
-//         let merchantName = element.textContent;
-//         let href = element.getAttribute('href');
-//         let merchantId = href.split('/').pop();
+//     for(let mutation of mutationsList) {
+//         if (mutation.type === 'childList') {
+//             let element = document.querySelector('body > div.cp-cockpit.ng-scope > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(3) > div > div:nth-child(2) > div.cp-value > span > a');
+//             if (element) {
+//                 let href = element.getAttribute('href');
+//                 let merchantId = href.split('/').pop();
 
-//         // Store the merchant name and ID found on the page to chrome storage
-//         chrome.storage.local.set({merchantName: merchantName, merchantId: merchantId});
-        
-//         // Check if we have additional information for this merchant
-//         const info = data[merchantId];
-//         if (info) {
-//           // If we do, show the popup
-//           showPopup(info);
+//                 // Check if the merchantId is in the CSV data
+//                 let info = data.find(row => row.merchant_id === merchantId);
+//                 if (info) {
+//                     // If info exists for this merchant ID, create a popup
+//                     var popupDiv = document.createElement('div');
+//                     popupDiv.className = 'popupClass'; // add a class name to div for styling
+
+//                     // Create a div to hold the data
+//                     var infoDiv = document.createElement('div');
+//                     infoDiv.className = 'infoClass';
+
+//                     // Assume `info` is an object like { merchant_id: '123', name: 'Merchant name', field1: 'data1', field2: 'data2', ... }
+//                     for (let key in info) {
+//                         if (info.hasOwnProperty(key)) {
+//                             let keyValueDiv = document.createElement('div');
+//                             keyValueDiv.className = 'keyValueClass';
+
+//                             let keyDiv = document.createElement('div');
+//                             keyDiv.textContent = key;
+//                             keyDiv.className = 'keyClass';
+//                             keyValueDiv.appendChild(keyDiv);
+
+//                             let valueDiv = document.createElement('div');
+//                             valueDiv.textContent = info[key];
+//                             valueDiv.className = 'valueClass';
+//                             keyValueDiv.appendChild(valueDiv);
+
+//                             infoDiv.appendChild(keyValueDiv);
+//                         }
+//                     }
+
+//                     popupDiv.appendChild(infoDiv);
+
+//                     popupDiv.style.position = 'fixed';
+//                     popupDiv.style.top = '10px';
+//                     popupDiv.style.right = '10px';
+//                     popupDiv.style.backgroundColor = 'rgb(186 151 255 / 80%)';
+//                     popupDiv.style.padding = '20px';
+//                     popupDiv.style.border = '2px';
+//                     popupDiv.style.zIndex = 9999;
+//                     popupDiv.style.position = 'fixed';
+//                     popupDiv.style.boxShadow = 'rgb(0 0 0 / 29%) 6px 10px 14px 2px';
+//                     popupDiv.style.borderRadius = '5px';
+//                     popupDiv.style.height = '250px';
+//                     popupDiv.style.width = '350px';
+//                     popupDiv.style.color = 'white';
+//                     popupDiv.style.backdropFilter = 'blur(1px)';
+
+//                     // Create a close button
+//                     var closeButton = document.createElement('button');
+//                     closeButton.textContent = 'x';
+//                     closeButton.style.position = 'absolute';
+//                     closeButton.style.top = '0';
+//                     closeButton.style.right = '0';
+//                     closeButton.style.backgroundColor = 'transparent';
+//                     closeButton.style.border = 'none';
+//                     closeButton.style.cursor = 'pointer';
+//                     closeButton.style.fontSize = '15px';
+//                     // Add the click event listener to the close button
+//                     closeButton.addEventListener('click', function() {
+//                         document.body.removeChild(popupDiv);
+//                     });
+
+//                     // Add the close button to the popup div
+//                     popupDiv.appendChild(closeButton);
+
+//                     // Add the popup div to the page
+//                     document.body.appendChild(popupDiv);
+
+//                     // Once we've found the element and displayed the popup, we don't need to observe anymore
+//                     observer.disconnect();
+//                     break;
+//                 }
+//             }
 //         }
-
-//         // Once we've found the element, we don't need to observe anymore
-//         observer.disconnect();
-//         break;
-//       }
 //     }
-//   }
 // });
 
 // observer.observe(document.body, { childList: true, subtree: true });
-
-// function showPopup(info) {
-//   // Create a popup div
-//   var popupDiv = document.createElement('div');
-//   popupDiv.textContent = info;
-//   popupDiv.style.position = 'fixed';
-//   popupDiv.style.top = '10px';
-//   popupDiv.style.right = '10px';
-//   popupDiv.style.backgroundColor = 'rgb(186 151 255 / 80%)';
-//   popupDiv.style.padding = '20px';
-//   popupDiv.style.border = '2px solid white';
-//   popupDiv.style.zIndex = 9999;
-//   popupDiv.style.boxShadow = 'rgb(0 0 0 / 29%) 6px 10px 14px 2px';
-//   // '3px 5px 6px 1px rgba(6, 5, 23, 0.26)';
-//   popupDiv.style.borderRadius = '5px';
-//   popupDiv.style.height = '250px';
-//   popupDiv.style.width = '350px';
-//   popupDiv.style.color = 'white';
-//   popupDiv.style.backdropFilter = 'blur(1px)';
-
-
-//   // Create a close button
-//   var closeButton = document.createElement('button');
-//   closeButton.textContent = 'x';
-//   closeButton.style.position = 'absolute';
-//   closeButton.style.top = '0';
-//   closeButton.style.right = '0';
-//   closeButton.style.backgroundColor = 'transparent';
-//   closeButton.style.border = 'none';
-//   closeButton.style.cursor = 'pointer';
-//   closeButton.style.fontSize = '15px';
-
-//   // Add the click event listener to the close button
-//   closeButton.addEventListener('click', function() {
-//     document.body.removeChild(popupDiv);
-//   });
-
-//   // Add the close button to the popup div
-//   popupDiv.appendChild(closeButton);
-
-//   // Add the popup div to the page
-//   document.body.appendChild(popupDiv);
-// }
-
-
-
-
-
-// DO NOT UNCOMMENT 
-
-// try 3 WINNER WINNER CHICKEN DINNER
-// let observer = new MutationObserver((mutationsList, observer) => {
-//   for(let mutation of mutationsList) {
-//     if (mutation.type === 'childList') {
-//       let element = document.querySelector('body > div.cp-cockpit.ng-scope > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(3) > div > div:nth-child(2) > div.cp-value > span > a');
-//       if (element) {
-//         let merchantName = element.textContent;
-
-//         // Store the merchant name found on the page to chrome storage
-//         chrome.storage.local.set({merchantName: merchantName});
-        
-//         // Once we've found the element, we don't need to observe anymore
-//         observer.disconnect();
-//         break;
-//       }
-//     }
-//   }
-// });
-
-// observer.observe(document.body, { childList: true, subtree: true });
-
-
-
-
-
-
