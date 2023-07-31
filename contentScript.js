0fae8b4b0c3c15bc5220dd8d272c4f3946f34b3a
zeepc <zee.perez-canals@riskified.com>
2023-07-31 13:15:34 -0400

    change field mapping

--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,16 @@
+// Predtermine mapping of fields for UI
 
-// parsing CSV, Observer, Display data 
+
+let fieldNamesMap = {
+    "merchant_id": "Merchant ID",
+    "merchant_name": "Merchant Name",
+    "shipping_exceptions": "Shipping Exceptions",
+    "chargaback_notice_reason_code": "CHB Notices and Reason Codes",
+    "other": "Notes",
+    // etc.
+};
 
 // Fetch and parse CSV
-
 let data;
 fetch(chrome.runtime.getURL('exceptions.csv'))
     .then(response => response.text())
@@ -18,7 +26,6 @@ let observer = new MutationObserver((mutationsList, observer) => {
                 let merchantId;
                 for (let element of elements) {
                     let href = element.getAttribute('href');
-                    // Check if href is of the form `https://ngadmin.riskxint.com/admin/ngadmin/#/merchants/<merchantId>`
                     if (href.includes('/merchants/')) {
                         merchantId = href.split('/').pop();
                     } else {
@@ -26,27 +33,23 @@ let observer = new MutationObserver((mutationsList, observer) => {
                     }
                 }
 
-                // Check if the merchantId is in the CSV data
                 let info = data.find(row => row.merchant_id === merchantId);
                 if (info) {
-                    // If info exists for this merchant ID, create a popup
                     var popupDiv = document.createElement('div');
-                    popupDiv.className = 'popupClass'; // add a class name to div for styling
+                    popupDiv.className = 'popupClass';
 
-                    // Create a div to hold the data
                     var infoDiv = document.createElement('div');
                     infoDiv.className = 'infoClass';
 
-                    // Assume `info` is an object like { merchant_id: '123', name: 'Merchant name', field1: 'data1', field2: 'data2', ... }
                     for (let key in info) {
-                        if (info.hasOwnProperty(key) && info[key] !== null && info[key] !== '') { // Check if value is not null or empty
+                        if (info.hasOwnProperty(key) && info[key] !== null && info[key] !== '') {
                             let keyValueDiv = document.createElement('div');
-                            keyValueDiv.className = 'keyValueClass';                                    
+                            keyValueDiv.className = 'keyValueClass';                
 
                             let keyDiv = document.createElement('div');
-                            keyDiv.textContent = key;
-                            keyDiv.className = key; // here we assign the class name to be the key itself
-                            keyValueDiv.appendChild(keyDiv);                                    
+                            keyDiv.textContent = fieldNamesMap[key] || key; // Use the mapped name if it exists, otherwise use the key itself
+                            keyDiv.className = key;
+                            keyValueDiv.appendChild(keyDiv);                                   
 
                             let valueDiv = document.createElement('div');
                             valueDiv.textContent = info[key];
@@ -56,14 +59,11 @@ let observer = new MutationObserver((mutationsList, observer) => {
                             infoDiv.appendChild(keyValueDiv);
                         }
                     }
-                    
 
                     popupDiv.appendChild(infoDiv);
-                    // styler to be moved overto style sheet
                     popupDiv.style.minHeight = '250px'; // Minimum height
                     popupDiv.style.maxHeight = '90vh'; // Maximum height, 90% of the viewport height
                     popupDiv.style.overflowY = 'auto'; // Add a scrollbar if the content exceeds the max height
-
                     popupDiv.style.position = 'fixed';
                     popupDiv.style.top = '10px';
                     popupDiv.style.right = '10px';
@@ -74,8 +74,6 @@ let observer = new MutationObserver((mutationsList, observer) => {
                     popupDiv.style.position = 'fixed';
                     popupDiv.style.boxShadow = 'rgb(0 0 0 / 29%) 6px 10px 14px 2px';
                     popupDiv.style.borderRadius = '5px';
-                    // popupDiv.style.height = '250px';
-                    // popupDiv.style.width = '350px';
                     popupDiv.style.color = 'white';
                     popupDiv.style.backdropFilter = 'blur(1px)';
 
@@ -89,6 +87,7 @@ let observer = new MutationObserver((mutationsList, observer) => {
                     closeButton.style.border = 'none';
                     closeButton.style.cursor = 'pointer';
                     closeButton.style.fontSize = '15px';
+
                     // Add the click event listener to the close button
                     closeButton.addEventListener('click', function() {
                         document.body.removeChild(popupDiv);
@@ -110,6 +109,5 @@ let observer = new MutationObserver((mutationsList, observer) => {
 });
 
 observer.observe(document.body, { childList: true, subtree: true });
-
 
 
